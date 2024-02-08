e7e39703acc837ae7aedd2f341eb8bb0b800cf72
NadinDesyatova <nadindinina@yandex.ru>
2024-02-08 20:31:58 +0300

    Browser creation error has been fixed

--- scrapping.py
+++ scrapping.py
@@ -1,12 +1,11 @@
 import json
 import re
+
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 def wait_element(browser, delay_seconds=1, by=By.TAG_NAME, value=None):
     return WebDriverWait(browser, delay_seconds).until(
@@ -14,37 +13,35 @@ def wait_element(browser, delay_seconds=1, by=By.TAG_NAME, value=None):
     )
 
 
-path = ChromeDriverManager().install()
-browser_service = Service(executable_path=path)
-browser = Chrome(service=browser_service)
+browser = Chrome()
 
 parsed_data = []
 
 def get_vacancy_informaition(vacancies_link, browser):
     browser.get(vacancies_link)
-    main_serp_content = wait_element(browser, 1, By.CLASS_NAME, "vacancy-serp-content")
-    vacancy_list = main_serp_content.find_element(By.ID, "a11y-main-content")
-    for vacancy_tag in vacancy_list.find_elements(By.CLASS_NAME, "serp-item"):
+    serp_content = wait_element(
+        browser, 1, By.CLASS_NAME, "vacancy-serp-content"
+    )
+    vacancy_list = serp_content.find_elements(By.CLASS_NAME, "serp-item")
+    for vacancy_tag in vacancy_list:
         h3_tag = wait_element(vacancy_tag, 1, By.TAG_NAME, "h3")
         a_tag = h3_tag.find_element(By.TAG_NAME, "a")
         vacancy_name_tag = a_tag.find_element(By.TAG_NAME, "span")
-        employer_tag = vacancy_tag.find_element(By.CLASS_NAME, "bloko-link bloko-link_kind-tertiary")
-        address_tag = vacancy_tag.find_element(By.CLASS_NAME, "vacancy-serp__vacancy-address")
-        compensation_tag = vacancy_tag.find_element(By.CLASS_NAME, "bloko-header-section-2")
+        employer_tag = vacancy_tag.find_element(By.CLASS_NAME, "bloko-link")
+        address_tag = vacancy_tag.find_elements(By.CLASS_NAME, "bloko-text")[1]
+        try:
+            compensation_tag = vacancy_tag.find_element(By.CLASS_NAME, "bloko-header-section-2")
+            vacancy_compensation = compensation_tag.text
+        except NoSuchElementException:
+            vacancy_compensation = "Не указано"
 
         vacancy_employer = employer_tag.text
         vacancy_address = address_tag.text
         link_absolute = a_tag.get_attribute("href")
         vacancy_name = vacancy_name_tag.text
 
-        if compensation_tag is not None:
-            vacancy_compensation = compensation_tag.text
-        else:
-            vacancy_compensation = "Не указано"
-
-        if (re.findall(r"$", vacancy_compensation)
-                and (re.findall(r"django", vacancy_name, re.I|re.M)
-                or re.findall(r"flask", vacancy_name, re.I|re.M))):
+        if (re.findall(r"django", vacancy_name, re.I | re.M)
+                or re.findall(r"flask", vacancy_name, re.I | re.M)):
             parsed_data.append(
                 {
                     "employer": vacancy_employer,
@@ -54,26 +51,10 @@ def get_vacancy_informaition(vacancies_link, browser):
                 }
             )
 
-    pager_tag = main_serp_content.find_element(By.CLASS_NAME, "pager")
-    next_page_button_tags = pager_tag.find_elements(By.TAG_NAME, "a")
-    for link_tag in next_page_button_tags:
-        if link_tag.find_element(By.LINK_TEXT, "дальше") is not None:
-            next_page_link = link_tag.get_attribute("href")
-            break
-        else:
-            next_page_link = None
-
-
-    return next_page_link
+    print('Парсинг завершен')
 
 vacancies_link = "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
-next_page = get_vacancy_informaition(vacancies_link, browser)
-
-flag = True
-while flag:
-    next_page = get_vacancy_informaition(next_page, browser)
-    if next_page is None:
-        flag = False
+get_vacancy_informaition(vacancies_link, browser)
 
 with open("vacancies_file.json", "w", encoding="utf-8") as f:
     json.dump(parsed_data, f, ensure_ascii=False, indent=2)
